410a4e2589c5a090566fd75007d4a1ae1bec885a
andy3471 <andy@insideindie.net>
2020-02-07 10:11:37 +0000

    Quasar Build Fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,6 @@ const mix = require("laravel-mix");
  |
  */
 
-//mix.js('resources/js/app.js', 'public/js')
-//    .sass('resources/sass/app.scss', 'public/css');
-
 mix.copy(
     "client/dist/spa/index.html",
     "resources/views/app.blade.php"
